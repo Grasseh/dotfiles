76f60e50cd974f27045b786c9d9c738c36b5cb38
Grasseh <steve.gagnev4si@gmail.com>
2016-03-31 00:33:29 -0400

    Added basic files with links. Added Vim.

---
+++ init.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo "Loading Configuration Dotfiles!"
+
+echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
+
+echo "Installing missing programs"
+
+if ! command -v "vim" > /dev/null/; then
+    echo " Vim is not installed." 
+    echo "Installing Vim"
+    sudo apt-get install vim
+else
+    echo "Vim is already installed!"
+fi
+
+echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
+
+echo "Creating file symlinks"
+
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig;
+echo "Done linking Global Git Config"
+
+ln -sf ~/dotfiles/.gitignore ~/.gitignore;
+echo "Done linking Global Git Ignore"
+
+ln -sf ~/dotfiles/.bash_profile ~/.bash_profile;
+echo "Done linking Bash Profile"  
+
+echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
+
+echo "Done writing configurations!"
